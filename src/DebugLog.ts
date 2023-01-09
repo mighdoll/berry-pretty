137a62a8ec9231306077071bcba1020268de8979
lee mighdoll <lee@underneath.ca>
2023-01-08 21:48:13 -0800

    tweak name and version for initial publish

--- src/DebugLog.ts
+++ src/DebugLog.ts
@@ -4,13 +4,7 @@ import { callerName } from "./BerryStack";
 import { indentMultiLine, padTrimCaller } from "./BerryStringUtil";
 import { replaceUndefined } from "./ReplaceUndefined";
 
-/** options for dlog and friends */
 export type DebugLogOptions = BerryPrettyOptions & LogOptions;
-
-/** alternate spellings for intercap fans */
-export const dLog = dlog;
-export const dErr = derr;
-export const dWarn = dwarn;
 
 /** options for debugLog not used by BerryPretty */
 interface LogOptions {
@@ -31,16 +25,17 @@ const defaultOptions: Required<DebugLogOptions> = {
   precision: 4,
 };
 
+export const dlog = dLog;
+export const derr = dErr;
+export const dwarn = dWarn;
+
 /** concise debug logging */
-export function dlog(...items: any[]): void {
+export function dLog(...items: any[]): void {
   console.log(...dLogMessages({}, ...items));
 }
 
 /** concise debug logging, with options */
-export function dlogOpt(
-  options: DebugLogOptions,
-  ...items: [any, ...any]
-): void {
+export function dlogOpt(options: DebugLogOptions, ...items: [any, ...any]): void {
   // TODO this needs to be multiline aware, the second item doesn't know where it's beginning
   console.log(...dLogMessages(options, ...items));
 }
@@ -54,12 +49,12 @@ export function dsert(test: boolean, ...items: any[]): void {
 }
 
 /** concise error logging */
-export function derr(...items: any[]): void {
+export function dErr (...items: any[]): void {
   console.error(...dLogMessages({}, ...items));
 }
 
 /** concise warning logging */
-export function dwarn(...items: any[]): void {
+export function dWarn(...items: any[]): void {
   console.warn(...dLogMessages({}, ...items));
 }
 
@@ -69,10 +64,7 @@ export function dLogDefaultOptions(options: DebugLogOptions): void {
 }
 
 /** exported for testing */
-export function dlogMessagesTest(
-  options: DebugLogOptions,
-  ...items: any[]
-): any[] {
+export function dlogMessagesTest(options: DebugLogOptions, ...items: any[]): any[] {
   return dLogMessages(options, ...items);
 }
 
@@ -94,10 +86,7 @@ function dLogMessages(options: DebugLogOptions, ...items: any[]): any[] {
 }
 
 /** @return a string for convenient debug logging some variables. */
-function debugVars(
-  vars: Record<string, unknown>,
-  options?: BerryPrettyOptions
-): string {
+function debugVars(vars: Record<string, unknown>, options?: BerryPrettyOptions): string {
   const strings = Object.entries(vars).map(
     ([key, value]) => `${key}: ${pretty(value, options)}`
   );
